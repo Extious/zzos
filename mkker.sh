ddd10c937ef4cb90f378959553e29773238adc3c
eiclpy <pengyu.l@foxmail.com>
2021-08-23 14:18:35 +0800

    auto get latest kernel

--- mkker.sh
+++ mkker.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-version=${1:-5.13.11}
-kernelxz=linux-$version.tar.xz
-kerneldir=linux-$version
+kernel_url=$(curl -s https://www.kernel.org/ | grep -Pzo "latest_link.*\n.*$" | grep href | awk -F \" '{print $2}')
+kernel_fn=${kernel_url##*/}
+kernel_dir=${kernel_fn%.tar*}
 
-[ -e $kernelxz ] || wget https://cdn.kernel.org/pub/linux/kernel/v5.x/$kernelxz
-[ -d $kerneldir ] || tar xJf $kernelxz
-cp .config $kerneldir/
-cd $kerneldir
+if ! [ -e $kernel_fn ]; then
+    echo Downloading $kernel_url
+    wget $kernel_url
+    rm -rf $kernel_dir
+fi
+[ -d $kernel_dir ] || tar xJf $kernel_fn
+cp .config $kernel_dir/
+cd $kernel_dir
 make -j64
 cd -
-cp $kerneldir/arch/x86/boot/bzImage .
+cp $kernel_dir/arch/x86/boot/bzImage .
